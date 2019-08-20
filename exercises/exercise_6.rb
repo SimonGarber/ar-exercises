a8ae4681ea3e1b5249550ee15a4d3f7e19bdb1a8
simongarber <simon.garber@gmail.com>
2019-08-20 23:17:16 +0000

    create one to many associations for the employee and store classes. Made a number of employee record insert statements into the employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,21 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store 
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :Store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 35)
+@store1.employees.create(first_name: "Simon", last_name: "Garber", hourly_rate: 125)
+@store1.employees.create(first_name: "Jack", last_name: "Johnston", hourly_rate: 50)
+@store2.employees.create(first_name: "Spiros", last_name: "Jackson", hourly_rate: 70)
+@store2.employees.create(first_name: "Abe", last_name: "Vagoda", hourly_rate: 88)
+@store2.employees.create(first_name: "Michael", last_name: "Chan", hourly_rate: 42)
+@store2.employees.create(first_name: "Bill", last_name: "Preston", hourly_rate: 69)
+@store2.employees.create(first_name: "Theodore", last_name: "Logan", hourly_rate: 69)
+@store2.employees.create(first_name: "Charles", last_name: "Roberts", hourly_rate: 50)
