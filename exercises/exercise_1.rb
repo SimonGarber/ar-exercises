106f2dd5968f8198ca4b2cc9bbaac94355f321db
simongarber <simon.garber@gmail.com>
2019-08-20 20:54:32 +0000

    added the active record insert statements to create 3 stores and save that data to the database as well as outputting the count of all the stores to the console

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,25 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+t = Store.new
+t.name = "Burnaby"
+t.annual_revenue = 3000000
+t.womens_apparel = true
+t.mens_apparel = true
+t.save
+
+t = Store.new
+t.name = "Richmond"
+t.annual_revenue = 1260000
+t.womens_apparel = true
+t.mens_apparel = false
+t.save
+
+t = Store.new
+t.name = "Gastown"
+t.annual_revenue = 190000
+t.womens_apparel = false
+t.mens_apparel = true
+t.save
+
+puts Store.count
